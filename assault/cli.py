0808ee6f045c6fbd2c21ebc998ba01e167e4669b
Debabrata Poddar <debobratapoddar3@gmail.com>
2019-09-14 04:55:15 +0000

    Assault pip install executable cli making

---
+++ assault/cli.py
@@ -0,0 +1,17 @@
+import click
+
+
+@click.command()
+@click.option("--request", "-r", default=500, help="Number of requests")
+@click.option("--concurrency", "-c", default=1, help="Number of concurrent requests")
+@click.option("--json-file", "-j", default=None, help="Output JSON file path")
+@click.argument("url")
+def cli(request, concurrency, json_file, url):
+    print(f"Requests: {request}")
+    print(f"Concurrency: {concurrency}")
+    print(f"JSON-File: {json_file}")
+    print(f"Url: {url}")
+
+
+if __name__ == "__main__":
+    cli()
