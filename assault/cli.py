8fa5f72013659de74c8ea352e192df974550f21f
Debabrata Poddar <debobratapoddar3@gmail.com>
2019-09-14 13:14:52 +0000

    Result has been presented in a json file

--- assault/cli.py
+++ assault/cli.py
@@ -1,6 +1,10 @@
 import click
+import sys
+import json
+from typing import TextIO
 
 from .http_request import assault
+from .stats import Results
 
 
 @click.command()
@@ -9,12 +13,41 @@ from .http_request import assault
 @click.option("--json-file", "-j", default=None, help="Output JSON file path")
 @click.argument("url")
 def cli(request, concurrency, json_file, url):
-    print(f"Requests: {request}")
-    print(f"Concurrency: {concurrency}")
-    print(f"JSON-File: {json_file}")
-    print(f"Url: {url}")
-    assault(url, request, concurrency)
+    output_file = None
+    if json_file:
+        try:
+            output_file = open(json_file, "w")
+        except:
+            print(f"Unable to open file {json_file}")
+            sys.exit(1)
+    total_time, request_dict = assault(url, request, concurrency)
+    results = Results(total_time, request_dict)
+    display(results, output_file)
 
 
-if __name__ == "__main__":
-    cli()
+def display(results, json_file: TextIO):
+    if json_file:
+        # Write to a file
+        json.dump(
+            {
+                "successful_requests": results.successful_requests(),
+                "slowest": results.slowest(),
+                "fastest": results.fastest(),
+                "total_time": results.total_time,
+                "requests_per_minute": results.requests_per_minute(),
+                "requests_per_second": results.requests_per_second(),
+            },
+            json_file,
+        )
+        json_file.close()
+        print(".... Done!")
+    else:
+        # Print to screen
+        print(".... Done!")
+        print("--- Results ---")
+        print(f"Successful Requests\t{results.successful_requests()}")
+        print(f"Slowest            \t{results.slowest()}s")
+        print(f"Fastest            \t{results.fastest()}s")
+        print(f"Total time         \t{results.total_time}s")
+        print(f"Requests Per Minute\t{results.requests_per_minute()}")
+        print(f"Requests Per Second\t{results.requests_per_second()}")
